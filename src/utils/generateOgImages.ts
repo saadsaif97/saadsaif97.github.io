0a6a6b9592b0f269d9f0b995e5d290a217f6c3de
saadsaif97 <saadgfx97@gmail.com>
2026-08-25 22:43:12 +0500

    shopify ai training

--- src/utils/generateOgImages.ts
+++ src/utils/generateOgImages.ts
@@ -2,7 +2,6 @@ import { Resvg } from "@resvg/resvg-js";
 import { type CollectionEntry } from "astro:content";
 import postOgImage from "./og-templates/post";
 import siteOgImage from "./og-templates/site";
-import trainingOgImage from "./og-templates/training";
 
 function svgBufferToPngBuffer(svg: string) {
   const resvg = new Resvg(svg);
@@ -17,10 +16,5 @@ export async function generateOgImageForPost(post: CollectionEntry<"blog">) {
 
 export async function generateOgImageForSite() {
   const svg = await siteOgImage();
-  return svgBufferToPngBuffer(svg);
-}
-
-export async function generateOgImageForTraining() {
-  const svg = await trainingOgImage();
   return svgBufferToPngBuffer(svg);
 }
